11026febf5b396bde6fe3e191e5d7802efdc2292
Sea <xinghaiyang1994@sina.com>
2017-06-05 13:17:10 +0800

    complete

--- js/share.js
+++ js/share.js
@@ -1,19 +1,8 @@
-//需要引入jQuery
-//分享的节点需要设置data-share属性
-//data-share属性值:
-//	qzone			QQ空间
-//	weibo 			微博
-//	tweibo 			腾讯微博
-//	douban          豆瓣
-//	people          人人网
-
-//new Share({
-//	dom:[],				//数组，必填，存放触发事件的节点
-//	contentDom:''		//分享的内容节点
-//	title:''				//分享的标题
-//})
-
-
+/*
+ *  作者：1295306442@qq.com
+ *  	时间：2017-06-05
+ * 	描述：分享插件
+ */   
 
 (function(window,document,$){
 	
